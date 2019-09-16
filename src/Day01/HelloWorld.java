fd0d30b729dbc773a4d962779f89f3fb09b22a6b
water <1351118308@qq.com>
2019-09-16 23:02:36 +0800

    commit
    Change-Id: Id437ceff51d438abdf72db56d8ad0f117fc2679d

---
+++ src/Day01/HelloWorld.java
@@ -0,0 +1,20 @@
+package Day01;
+
+public class HelloWorld {
+
+/*
+	java执行原理
+	1、执行前通过编译器得到class文件
+	2、类加载器将class文件加载到虚拟机
+	3、Java虚拟机执行代码
+*/
+/*
+ * 关键字：Java中的具有特殊含义的单词，不能用来定义类
+ * */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	
+	
+	
+}
